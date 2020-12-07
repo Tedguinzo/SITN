44bda5e8f54ee9b43a17387d3d2a89e1b84486a8
Tedguinzo <75610635+Tedguinzo@users.noreply.github.com>
2020-12-07 11:58:47 +0100

    Add files via upload

---
+++ graphe.m
@@ -0,0 +1,11 @@
+function graphe(ypred)
+    global date
+    plot(date,ypred)
+    ylim([0,1])
+    xlabel("jours");
+    ylabel("% pop");
+    legend({"Sains","Infected","Recovered","Deaths"},'Location','best')
+    yyaxis right
+    ylabel("% pop");
+    set(gca,'ycolor','k') 
+end
